da6c4ba8770dfd8783d110df6d8f2ec9f4033cef
ClassUser <ClassUser@spbrtk.local>
2019-09-05 11:34:28 +0300

    xz chto

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'YjVMA5vVMhLn2OG0o3okow30wsVemJ7h',
+            'cookieValidationKey' => 'vj6ek1TFUOeURTm3OD9CSpTDN9IkTvfj',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
