2567ea5591e77d0f9d96dfb1a1c8d04d66f2c6f1
Tol-Mi <tol-mi@mail.ru>
2019-10-01 12:14:25 +0300

    back

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'vj6ek1TFUOeURTm3OD9CSpTDN9IkTvfj',
+            'cookieValidationKey' => 'YjVMA5vVMhLn2OG0o3okow30wsVemJ7h',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
